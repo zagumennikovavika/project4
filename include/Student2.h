d8ed534604271c4a7c76b225470bc2858c32d445
d-IT-tO <bykovivan073@gmail.com>
2026-03-27 11:37:07 +0700

    Create Student2.h

---
+++ include/Student2.h
@@ -0,0 +1,41 @@
+#ifndef STUDENT2_H
+#define STUDENT2_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_NODE_SIZE 128
+
+typedef struct ExprNode{
+    NodeType type;
+    union{
+        double number;
+        char* var_name;
+        struct{
+            struct EXprNode* operand;
+            char op;
+        } unary;
+        struct{
+            struct ExprNode* left;
+            struct ExprNode* right;
+            char op;
+        } binary;
+        struct{
+            struct ExprNode** args;
+            int arg_count;
+            char* func_name;
+        } function;
+    } data;
+} ExprNode;
+
+typedef struct{
+    char* name;
+    double var;
+} VariableTable;
+
+ExprNode* build_ast_from_postfix(const char* postfix);
+double evaluate_ast(ExprNode *node, VariableTable* variables);
+void free_ast(ExprNode* node);
+
+#endif
